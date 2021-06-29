86ddbf0d68d808fffc4a64c84772d935c6267872
Bobby Reay <rreay724@gmail.com>
2021-06-29 16:25:39 -0400

    installed tailwind, created visual piece of login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -20,8 +20,56 @@ function Login() {
   }, []);
 
   return (
-    <div>
-      <p>Login Page</p>
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img
+          src="/images/iphone-with-profile.jpeg"
+          alt="iPhone"
+          className="max-w-full"
+        />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <div className="flex flex-col  items-center bg-white p-4 border border-gray-primary mb-4">
+          <h1 className="flex justify-center w-full">
+            <img
+              className="mt-2 w-6/12 mb-4"
+              src="/images/logo.png"
+              alt="instagram logo"
+            />
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border 
+            border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border 
+            border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${
+                isInvalid && "opacity-50"
+              }`}
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
+          <p className="text-sm">Don't have an account?</p>
+        </div>
+      </div>
     </div>
   );
 }
