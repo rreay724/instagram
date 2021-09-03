4f06972b495b8824db99fcb2adc796c482cc0a3b
Bobby Reay <rreay724@gmail.com>
2021-09-03 14:37:00 -0400

    fixed login signup page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -59,7 +59,7 @@ function SignUp() {
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <div className="sm:flex w-3/5 hidden">
+      <div className="sm:flex w-3/5 ">
         <img
           src="/images/iphone-with-profile.jpeg"
           alt="iPhone"
