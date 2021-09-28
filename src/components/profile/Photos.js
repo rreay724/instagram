98b67a20049b5ebb2197713c0e074b9c9fa39b1e
Bobby Reay <rreay724@gmail.com>
2021-09-28 12:07:37 -0400

    added padding to bottom of photos

--- src/components/profile/Photos.js
+++ src/components/profile/Photos.js
@@ -3,8 +3,8 @@ import Skeleton from "react-loading-skeleton";
 
 export default function Photos({ photos }) {
   return (
-    <div className="h-16 border-t border-gray-primary mt-12 pt-4">
-      <div className="grid grid-cols-3 gap-8 mt-4 mb-12">
+    <div className="h-16 border-t border-gray-primary mt-12 pt-4 ">
+      <div className="grid grid-cols-3 gap-8 mt-4 mb-12 pb-24">
         {!photos
           ? new Array(12)
               .fill(0)
