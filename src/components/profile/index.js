ade5ac55bc3604d5789bfbdc71d438b256f91425
Bobby Reay <rreay724@gmail.com>
2021-09-29 14:20:17 -0400

    f

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -26,8 +26,7 @@ export default function UserProfile({ user }) {
     if (user.username) {
       getProfileInfoAndPhotos();
     }
-  }, []);
-  // }, [user.username]);
+  }, [user.username]);
 
   return (
     <div>
